24a092347b8363bdd54beda9e846440dfc4f20fa
Mike Lambert <lambert8@illinois.edu>
2017-04-26 11:56:18 -0500

    first commit

---
+++ src/main/java/org/nationaldataservice/elasticsearch/queryexpansion/App.java
@@ -0,0 +1,13 @@
+package org.nationaldataservice.elasticsearch.queryexpansion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
