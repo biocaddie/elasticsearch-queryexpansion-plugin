878f82a651c515493c7eeef99cf578c1a3ca2341
Mike Lambert <lambert8@illinois.edu>
2017-04-28 14:58:21 -0500

    Fixed compilation error

--- src/main/java/org/nationaldataservice/elasticsearch/queryexpansion/RestQueryExpansionAction.java
+++ src/main/java/org/nationaldataservice/elasticsearch/queryexpansion/RestQueryExpansionAction.java
@@ -2,13 +2,14 @@ package org.nationaldataservice.elasticsearch.queryexpansion;
 
 import static org.elasticsearch.rest.RestRequest.Method.GET;
 
+import java.io.IOException;
+
 import org.elasticsearch.client.node.NodeClient;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.xcontent.XContentFactory;
 import org.elasticsearch.rest.BaseRestHandler;
 import org.elasticsearch.rest.BytesRestResponse;
-import org.elasticsearch.rest.RestChannel;
 import org.elasticsearch.rest.RestController;
 import org.elasticsearch.rest.RestRequest;
 import org.elasticsearch.rest.RestStatus;
@@ -19,9 +20,9 @@ public class RestQueryExpansionAction extends BaseRestHandler {
 		super(settings);    
 		controller.registerHandler(GET, "/_ hello", this);
 	}
-
-    public void handleRequest(RestRequest request, RestChannel channel, NodeClient client) throws Exception {
-          String clustername = client.settings().get("cluster.name");
-          channel.sendResponse(new BytesRestResponse(RestStatus.OK, XContentFactory.jsonBuilder().startObject().field("hello", "This is cluster – " + clustername).endObject()));
-    }
+    @Override
+	protected RestChannelConsumer prepareRequest(RestRequest request, NodeClient client) throws IOException {
+        String clustername = client.settings().get("cluster.name");
+        return channel -> channel.sendResponse(new BytesRestResponse(RestStatus.OK, XContentFactory.jsonBuilder().startObject().field("hello", "This is cluster – " + clustername).endObject()));
+	}
 }
