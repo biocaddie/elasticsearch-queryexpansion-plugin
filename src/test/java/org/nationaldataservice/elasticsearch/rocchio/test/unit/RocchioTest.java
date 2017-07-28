bc4cb8fa20c723498a276b866d60c7c59696c896
Mike Lambert <lambert8@illinois.edu>
2017-07-28 02:30:38 -0500

    Use a static initializer to clean up messy mock code

--- src/test/java/org/nationaldataservice/elasticsearch/rocchio/test/unit/RocchioTest.java
+++ src/test/java/org/nationaldataservice/elasticsearch/rocchio/test/unit/RocchioTest.java
@@ -45,161 +45,162 @@ import edu.gslis.textrepresentation.FeatureVector;
 
 @RunWith(MockitoJUnitRunner.class)
 public class RocchioTest {
-	// TODO: Insert real/mocked settings here?
-	//private Settings settings = Settings.builder().build();
-	
-	// TODO: Mock these
-	private Client client = mock(Client.class);
-	
-	// Our common test parameter set (individual tests can still use one-off values)
+	/** The Rocchio instance to test */
+	private Rocchio rocchio;
+
+	// Mock out all of the ElasticSearch internals
+	private static final Client client = mock(Client.class);
+
+	// The common test parameter set (individual tests can still use one-off
+	// values)
 	private static String TEST_EXPAND_INDEX = "biocaddie";
 	private static String TEST_SEARCH_INDEX = "biocaddie";
 	private static String TEST_QUERY = "multiple sclerosis";
 	private static String TEST_TYPE = "dataset";
 	private static String TEST_FIELD = "_all";
-	private static int    TEST_FB_TERMS = 10;
-	private static int    TEST_FB_DOCS = 50;
+	private static int TEST_FB_TERMS = 10;
+	private static int TEST_FB_DOCS = 50;
 	private static double TEST_ALPHA = 0.5;
 	private static double TEST_BETA = 0.5;
 	private static double TEST_K1 = 1.2;
 	private static double TEST_B = 0.75;
-	
-	private Rocchio rocchio;
-	
-	/* This one is sort of a ridiculous rabbit hole to mock out... */
-	private void mockIndexMetaDataRequest() throws IOException {
-		AdminClient adminClient = mock(AdminClient.class);
-		when(client.admin()).thenReturn(adminClient);
-		
-		ClusterAdminClient clusterAdminClient = mock(ClusterAdminClient.class);
-		when(adminClient.cluster()).thenReturn(clusterAdminClient);
-		
-		ActionFuture<ClusterStateResponse> clusterStateFuture = (ActionFuture<ClusterStateResponse>) mock(ActionFuture.class);
-		when(clusterAdminClient.state(any())).thenReturn(clusterStateFuture);
-		
-		ClusterStateResponse clusterStateResponse = mock(ClusterStateResponse.class);
-		when(clusterStateFuture.actionGet()).thenReturn(clusterStateResponse);
-		
-		ClusterState clusterState = mock(ClusterState.class);
-		when(clusterStateResponse.getState()).thenReturn(clusterState);
-		
-		MetaData clusterMetadata = mock(MetaData.class);
-		when(clusterState.getMetaData()).thenReturn(clusterMetadata);
 
-		IndexMetaData mockIndexMetaData = mock(IndexMetaData.class);
-		when(clusterMetadata.index(anyString())).thenReturn(mockIndexMetaData);
+	// The index mapping metadata and sub-mappings
+	private static MappingMetaData mockTypeMetadata = mock(MappingMetaData.class);
+	private static final ImmutableOpenMap<String, MappingMetaData> indexMappingMetadata;
+	private static final LinkedHashMap<String, Object> fieldPropertiesMap = new LinkedHashMap<String, Object>();
+	private static final LinkedHashMap<String, Object> typePropertiesMap = new LinkedHashMap<String, Object>();
+	private static final LinkedHashMap<String, Object> typeMap = new LinkedHashMap<String, Object>();
+	private static final Map<String, MappingMetaData> typeMetadataMapping = new HashMap<>();
 
+	// FIXME: finish mocking out iterator and expand
+	private static final BytesRef ref = new BytesRef("asdf");
 
-		MappingMetaData mockTypeMetadata = mock(MappingMetaData.class);
-		Map<String, MappingMetaData> hashMap = new HashMap<>();
-		hashMap.put(TEST_TYPE, mockTypeMetadata);
-		when(mockIndexMetaData.getMappings()).thenReturn(new ImmutableOpenMap.Builder<String, MappingMetaData>().putAll(hashMap).build());
-		
-		LinkedHashMap<String, Object> fieldPropertiesMap = new LinkedHashMap<String, Object>();
+	// FIXME: Somehow get real values to return here for expand / search
+	private static final SearchHit[] hitsArray = new SearchHit[3];
+
+	@SuppressWarnings("unchecked")
+	private static final ActionFuture<ClusterStateResponse> clusterStateFuture = (ActionFuture<ClusterStateResponse>) mock(
+			ActionFuture.class);
+	private static final AdminClient adminClient = mock(AdminClient.class);
+	private static final ClusterAdminClient clusterAdminClient = mock(ClusterAdminClient.class);
+	private static final ClusterState clusterState = mock(ClusterState.class);
+	private static final ClusterStateResponse clusterStateResponse = mock(ClusterStateResponse.class);
+	private static final MetaData clusterMetadata = mock(MetaData.class);
+	private static final IndexMetaData mockIndexMetaData = mock(IndexMetaData.class);
+
+	@SuppressWarnings("unchecked")
+	private static final ListenableActionFuture<MultiTermVectorsResponse> mockMtvFuture = mock(
+			ListenableActionFuture.class);
+	private static final MultiTermVectorsResponse mockMtvResponse = mock(MultiTermVectorsResponse.class);
+	private static final TermVectorsResponse mockTvResponse = mock(TermVectorsResponse.class);
+	private static final MultiTermVectorsItemResponse mockMtvItemResponse = mock(MultiTermVectorsItemResponse.class);
+	private static final MultiTermVectorsRequestBuilder mockMtvBuilder = mock(MultiTermVectorsRequestBuilder.class);
+	private static final Fields mockFields = mock(Fields.class);
+	private static final Terms mockTerms = mock(Terms.class);
+	private static final MultiTermVectorsItemResponse[] mockMtvItemResponses = { mockMtvItemResponse };
+
+	// FIXME: finish mocking out iterator and expand
+	private static final TermsEnum mockIterator = mock(TermsEnum.class);
+
+	@SuppressWarnings("unchecked")
+	private static final ListenableActionFuture<SearchResponse> mockSearchFuture = mock(ListenableActionFuture.class);
+	private static final SearchRequestBuilder srBuilder = mock(SearchRequestBuilder.class);
+	private static final SearchResponse mockSearchResponse = mock(SearchResponse.class);
+
+	// FIXME: Somehow get real values to return here for expand / search
+	private static final SearchHits hits = mock(SearchHits.class);
+
+	static {
+		// Build up our properties mapping: { "store": true } object
 		fieldPropertiesMap.put("store", true);
-		
-		LinkedHashMap<String, Object> typePropertiesMap = new LinkedHashMap<String, Object>();
+
+		// Build up our test field mapping with the properties map
 		typePropertiesMap.put(TEST_FIELD, fieldPropertiesMap);
-		
-		LinkedHashMap<String, Object> typeMap = new LinkedHashMap<String, Object>();
+
+		// Build up our test type mapping from the test field mapping
 		typeMap.put("properties", typePropertiesMap);
 		typeMap.put("_all", fieldPropertiesMap);
-		
-		when(mockTypeMetadata.getSourceAsMap()).thenReturn(typeMap);
-		// FIXME: We can't mock final classes...
-		//ImmutableOpenMap<String, MappingMetaData> mockIndexMappingMetadata = (ImmutableOpenMap<String, MappingMetaData>) mock(ImmutableOpenMap.class);
-		
-	}
-	
-	private void mockTermVectorRequest() throws IOException {
-		MultiTermVectorsRequestBuilder multiTermVectorBuilder = mock(MultiTermVectorsRequestBuilder.class);
-		when(multiTermVectorBuilder.add(any())).thenReturn(multiTermVectorBuilder);
-		
-		MultiTermVectorsResponse mtvResponse = mock(MultiTermVectorsResponse.class);
-		
-		@SuppressWarnings("unchecked")
-		ListenableActionFuture<MultiTermVectorsResponse> mockedFuture = mock(ListenableActionFuture.class);
 
-		when(multiTermVectorBuilder.execute()).thenReturn(mockedFuture);
-		when(mockedFuture.actionGet()).thenReturn(mtvResponse);
-		
-		when(this.client.prepareMultiTermVectors()).thenReturn(multiTermVectorBuilder);
+		// Build up our test type mapping of the type metadata
+		typeMetadataMapping.put(TEST_TYPE, mockTypeMetadata);
 
-		TermVectorsResponse mockTvResponse = mock(TermVectorsResponse.class);
-		MultiTermVectorsItemResponse mockMtvItemResponse = mock(MultiTermVectorsItemResponse.class);
-		when(mockMtvItemResponse.getResponse()).thenReturn(mockTvResponse);
-		
-		MultiTermVectorsItemResponse[] mockMtvItemResponses = { mockMtvItemResponse };
-		when(mtvResponse.getResponses()).thenReturn(mockMtvItemResponses);
-		
-		Fields mockFields = mock(Fields.class);
-		when(mockTvResponse.getFields()).thenReturn(mockFields);
-		Terms mockTerms = mock(Terms.class);
-		when(mockFields.terms(TEST_FIELD)).thenReturn(mockTerms);
-		when(mockTerms.getDocCount()).thenReturn(10);
-		when(mockTerms.getSumTotalTermFreq()).thenReturn(10L);
-		
-		TermsEnum mockIterator = mock(TermsEnum.class);
-		when(mockTerms.iterator()).thenReturn(mockIterator);
+		// Build up our index mapping from the type mapping
+		indexMappingMetadata = new ImmutableOpenMap.Builder<String, MappingMetaData>().putAll(typeMetadataMapping)
+				.build();
 
-		when(mockIterator.next()).thenReturn(null);
-		when(mockIterator.totalTermFreq()).thenReturn(10L);
-		when(mockIterator.docFreq()).thenReturn(10);
-		
-		BytesRef ref = new BytesRef("asdf");
-		when(mockIterator.term()).thenReturn(ref);
-	}
-	
-	private void mockSearchRequest() {
-		SearchRequestBuilder srBuilder = mock(SearchRequestBuilder.class);
-		when(srBuilder.setQuery(any(QueryStringQueryBuilder.class))).thenReturn(srBuilder);
-		when(srBuilder.setSize(anyInt())).thenReturn(srBuilder);
-		
-		@SuppressWarnings("unchecked")
-		ListenableActionFuture<SearchResponse> mockedFuture = mock(ListenableActionFuture.class);
-		when(srBuilder.execute()).thenReturn(mockedFuture);
-		
-		// TODO: Somehow get the values to return here for expand / search
-		SearchHit[] hitsArray = new SearchHit[3];
+		// Build up our expected results from search
 		hitsArray[0] = mock(SearchHit.class);
 		hitsArray[1] = mock(SearchHit.class);
 		hitsArray[2] = mock(SearchHit.class);
-		
-		SearchHits hits = mock(SearchHits.class);
-		when(hits.getHits()).thenReturn(hitsArray);
-		when(hits.hits()).thenReturn(hitsArray);
-		when(hits.totalHits()).thenReturn(Long.valueOf(3));
-		when(hits.getTotalHits()).thenReturn(Long.valueOf(3));
-		
-		SearchResponse response = mock(SearchResponse.class);
-		when(mockedFuture.actionGet()).thenReturn(response);
-		when(response.getHits()).thenReturn(hits);
-		
-		when(this.client.prepareSearch(anyString())).thenReturn(srBuilder);
-	}
-	
+
+		try {
+			// Mock out all expected ElasticSearch responses
+			when(client.admin()).thenReturn(adminClient);
+			when(adminClient.cluster()).thenReturn(clusterAdminClient);
+			when(clusterAdminClient.state(any())).thenReturn(clusterStateFuture);
+			when(clusterStateFuture.actionGet()).thenReturn(clusterStateResponse);
+			when(clusterStateResponse.getState()).thenReturn(clusterState);
+			when(clusterState.getMetaData()).thenReturn(clusterMetadata);
+			when(clusterMetadata.index(anyString())).thenReturn(mockIndexMetaData);
+			when(mockIndexMetaData.getMappings()).thenReturn(indexMappingMetadata);
+			when(mockTypeMetadata.getSourceAsMap()).thenReturn(typeMap);
+
+			// Mock out all expected ElasticSearch responses
+			when(mockMtvBuilder.execute()).thenReturn(mockMtvFuture);
+			when(mockMtvFuture.actionGet()).thenReturn(mockMtvResponse);
+			when(mockMtvBuilder.add(any())).thenReturn(mockMtvBuilder);
+			when(client.prepareMultiTermVectors()).thenReturn(mockMtvBuilder);
+			when(mockMtvItemResponse.getResponse()).thenReturn(mockTvResponse);
+			when(mockMtvResponse.getResponses()).thenReturn(mockMtvItemResponses);
+
+			// FIXME: Mock out Lucene responses
+			when(mockTvResponse.getFields()).thenReturn(mockFields);
+			when(mockFields.terms(TEST_FIELD)).thenReturn(mockTerms);
+			when(mockTerms.getDocCount()).thenReturn(10);
+			when(mockTerms.getSumTotalTermFreq()).thenReturn(10L);
+			when(mockTerms.iterator()).thenReturn(mockIterator);
+
+			// FIXME: finish mocking out iterator and expand
+			when(mockIterator.next()).thenReturn(null);
+			when(mockIterator.totalTermFreq()).thenReturn(10L);
+			when(mockIterator.docFreq()).thenReturn(10);
+			when(mockIterator.term()).thenReturn(ref);
+
+			// Mock building our SearchRequest
+			when(client.prepareSearch(anyString())).thenReturn(srBuilder);
+			when(srBuilder.setQuery(any(QueryStringQueryBuilder.class))).thenReturn(srBuilder);
+			when(srBuilder.setSize(anyInt())).thenReturn(srBuilder);
+			when(srBuilder.execute()).thenReturn(mockSearchFuture);
+
+			// Mock performing our SearchRequest
+			when(mockSearchFuture.actionGet()).thenReturn(mockSearchResponse);
+			when(mockSearchResponse.getHits()).thenReturn(hits);
+
+			// Mock parsing the search results
+			when(hits.getHits()).thenReturn(hitsArray);
+			when(hits.hits()).thenReturn(hitsArray);
+
+			// FIXME: is this ok?
+			when(hits.totalHits()).thenReturn(Long.valueOf(3));
+			when(hits.getTotalHits()).thenReturn(Long.valueOf(3));
+		} catch (IOException e) {
+			e.printStackTrace();
+			fail();
+		}
+	};
+
 	@Before
 	public void setUp() throws IOException {
-		// FIXME: Can't mock this due to final class
-		mockIndexMetaDataRequest();
-		mockTermVectorRequest();
-		mockSearchRequest();
-		
 		// Initialize our Rocchio implementation (not mocked)
-		this.rocchio = createRocchio(TEST_EXPAND_INDEX, TEST_TYPE, TEST_FIELD, TEST_ALPHA, TEST_BETA, TEST_K1, TEST_B);
+		this.rocchio = new Rocchio(client, TEST_EXPAND_INDEX, TEST_TYPE, TEST_FIELD, TEST_ALPHA, TEST_BETA, TEST_K1, TEST_B);
 	}
-	
-	private Rocchio createRocchio(String index, String type, String field, double alpha, double beta, double k1, double b) {
-		return new Rocchio(client, index, type, field, alpha, beta, k1, b);
-		
-	}
-	
+
 	@After
 	public void tearDown() {
-		this.client = null;
 		this.rocchio = null;
 	}
-	
+
 	@Test
 	// Test that validate returns null if all parameters are valid
 	public void testValidate() throws IOException {
@@ -207,7 +208,7 @@ public class RocchioTest {
 		String shouldBeNull = rocchio.validate(TEST_QUERY, TEST_FB_DOCS, TEST_FB_TERMS);
 		assertNull(shouldBeNull);
 	}
-	
+
 	@Test
 	// Test that validate fails when query is null
 	public void testValidateInvalidQuery() throws IOException {
@@ -216,7 +217,7 @@ public class RocchioTest {
 		assertNotNull(errorMessage);
 		assertEquals(Rocchio.NULL_QUERY_ERROR, errorMessage);
 	}
-	
+
 	@Test
 	// Test that validate fails when fbDocs < 1
 	public void testValidateInvalidFeedbackDocuments() throws IOException {
@@ -225,7 +226,7 @@ public class RocchioTest {
 		assertNotNull(errorMessage);
 		assertEquals(Rocchio.INVALID_FB_DOCS_ERROR, errorMessage);
 	}
-	
+
 	@Test
 	// Test that validate fails when fbTerms < 1
 	public void testValidateInvalidFeedbackTerms() throws IOException {
@@ -234,14 +235,14 @@ public class RocchioTest {
 		assertNotNull(errorMessage);
 		assertEquals(Rocchio.INVALID_FB_TERMS_ERROR, errorMessage);
 	}
-	
+
 	@Test
 	// Test that we have correctly mocked runQuery
 	public void testRunQuery() {
 		SearchHits hits = rocchio.runQuery(TEST_SEARCH_INDEX, TEST_QUERY, TEST_FB_DOCS).getHits();
 		assertEquals(3, hits.getTotalHits());
 	}
-	
+
 	@Test
 	// Test that we can expand a query against the test index
 	public void testExpandQuery() throws IOException {
@@ -249,14 +250,14 @@ public class RocchioTest {
 		FeatureVector feedbackQuery = rocchio.expandQuery(TEST_QUERY, TEST_FB_DOCS, TEST_FB_TERMS);
 
 		// Format our expanded query with Lucene's boosting syntax
-		StringBuffer expandedQuery = new StringBuffer();
+		/*StringBuffer expandedQuery = new StringBuffer();
 		String separator = ""; // start out with no separator
 		for (String term : feedbackQuery.getFeatures()) {
 			expandedQuery.append(separator + term + "^" + feedbackQuery.getFeatureWeight(term));
 			separator = " "; // add separator after first iteration
-		}
-		
+		}*/
+
 		// FIXME: Regex?
-		assertEquals("multiple^", expandedQuery.toString());
+		assertEquals("multiple^", feedbackQuery.toString());
 	}
 }
